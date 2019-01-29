cc18db4131ee947523475ab879fb2c3240feea45
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-01-28 21:10:20 -0600

    First draft. Added display function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_tictac
 Tic tac using Python.
+
+Project to learn python.
+Creating a sample tictac game which can be player by 2 human players.
---
+++ tic_tac.py
@@ -0,0 +1,22 @@
+from IPython.display import clear_output
+
+
+'''
+Function will take a board list between index [0-9]
+It will display the board
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+display_board(test_board)
+Output :
+    X|O|X
+    -|-|-
+    O|X|O
+    -|-|-
+    X|O|X
+'''
+def display_board(board):
+    clear_output()
+    print(f"{board[1]}|{board[2]}|{board[3]}")
+    print("-|-|-")
+    print(f"{board[4]}|{board[5]}|{board[6]}")
+    print("-|-|-")
+    print(f"{board[7]}|{board[8]}|{board[9]}")
